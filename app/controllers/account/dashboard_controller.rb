905767391059240026facf6714ceeaf9fc077480
Carl Weis <carl@carlweis.com>
2016-06-30 15:50:30 -0500

    SAVEPOINT

---
+++ app/controllers/account/dashboard_controller.rb
@@ -0,0 +1,6 @@
+module Account
+  class DashboardController < ApplicationController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise routes
+  devise_for :users, path: 'account', path_names: {
+    sign_in:      'login',
+    sign_out:     'logout',
+    password:     'password',
+    confirmation: 'verification',
+    unlock:       'unblock',
+    sign_up:      'register'
+  }
+
+  # default route
   root 'pages#home'
+
+  # static page routes
   get 'tour' => 'pages#tour'
+
+  # User account routes
+  namespace :account do
+    get 'dashboard' => 'dashboard#index', as: 'dashboard'
+  end
+
 end
