7a613986c3df929112ef0fe611a23811c36f49d4
Carl Weis <carl@carlweis.com>
2016-07-09 03:19:27 -0500

    Switching back to erb

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,15 @@ Rails.application.routes.draw do
   # User account routes
   namespace :account do
     get 'dashboard' => 'dashboard#index', as: 'dashboard'
+
+    # prospect routes
     get 'prospects' => 'prospects#index', as: 'prospects'
     get 'prospects/:id' => 'prospects#show', as: 'prospect'
+
+    # mailing list routes
+    # get 'lists' => 'mailing_lists#index', as: 'mailing_lists'
+    # get 'lists/new' => 'mailing_lists#new', as: 'new_mailing_list'
+    resources :mailing_lists
   end
 
 end
