36b6a9a9bd6504432e9192937c16336214909711
Carl Weis <carl@carlweis.com>
2016-07-14 06:35:47 -0500

    Fix after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
 	end
 
   def after_sign_in_path_for(resource)
-   request.env['omniauth.origin'] || stored_location_for(resource) || account_dashboard_path || root_path
+   request.env['omniauth.origin'] || stored_location_for(resource) || dashboard_path || root_path
   end
 end
